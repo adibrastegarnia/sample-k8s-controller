9f52c40a83c70de2d1e346bd41119bb8c3e22716
adibrastegarnia <adib.rastegarnia@gmail.com>
2020-05-27 21:38:49 -0700

    Test

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,7 +1,13 @@
 package controller
 
 import (
+	"context"
+
+	"fmt"
+
 	_ "github.com/adibrastegarnia/sample-k8s-controller/pkg/apis/v1"
+	v1 "github.com/adibrastegarnia/sample-k8s-controller/pkg/apis/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -19,8 +25,18 @@ type Reconciler struct {
 // and what is in the Cluster.Spec
 func (r *Reconciler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 
-	//imageProcessing := v1.ImageProcessingClass{}
+	imageProcessing := &v1.ImageProcessingClass{}
+
+	err := r.client.Get(context.TODO(), request.NamespacedName, imageProcessing)
+	if err != nil {
+		log.Error(err, "Reconcile Database")
+		if k8serrors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{Requeue: true}, err
+	}
 	log.Info("Reconcile is called")
+	fmt.Println("image", imageProcessing.Spec.Image)
 
 	return reconcile.Result{}, nil
 
